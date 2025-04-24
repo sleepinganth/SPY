3af77b56985fecb6a0eaace277aedf99b666618b
Atrone <anthony.teixeira55@gmail.com>
2025-04-24 16:06:03 -0400

    a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 APP = ['spy_ema_chad.py']
-DATA_FILES = [('', ['__boot__.py'])]  # <- rename custom_bootstrap.py to __boot__.py
+DATA_FILES = [('', ['spy_ema_chad.py', '__boot__.py'])]  # <- rename custom_bootstrap.py to __boot__.py
 OPTIONS = {
     'argv_emulation': True,
     'packages': ['pandas', 'numpy', 'ib_insync', 'eventkit', 'nest_asyncio', 'dateutil', 'pytz'],
