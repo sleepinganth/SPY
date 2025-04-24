5c743bd29cc5557408cf275aa7495d1bc8fffadc
Atrone <anthony.teixeira55@gmail.com>
2025-04-24 14:49:47 -0400

    asf

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-# setup.py
 from setuptools import setup
 
 APP = ['spy_ema_chad.py']
 OPTIONS = {
     'argv_emulation': True,
-    'packages': [],
+    'packages': ['pandas', 'numpy', 'cmath'],
+    'includes': ['cmath'],
+    'excludes': [],
+    'plist': {
+        'CFBundleName': 'MacApp',
+    },
+    'optimize': 1,
+    'resources': [],  # Add any files needed
 }
 
 setup(
-    name='MacApp',
     app=APP,
+    name='MacApp',
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
