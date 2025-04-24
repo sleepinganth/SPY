b215c7e0611845805ee7f84ea3f564727c504920
Atrone <anthony.teixeira55@gmail.com>
2025-04-24 15:57:08 -0400

    a

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
 APP = ['spy_ema_chad.py']
-DATA_FILES = []
+DATA_FILES = [('', ['custom_bootstrap.py'])]
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['pandas', 'numpy', 'ib_insync'],
+    'packages': ['pandas', 'numpy', 'ib_insync', 'eventkit', 'nest_asyncio', 'dateutil', 'pytz'],
+    'includes': ['numpy.core.multiarray'],
     'excludes': ['wheel', 'setuptools._vendor.wheel'],
-    'site_packages': True,  # Use system site-packages rather than bundling
+    'site_packages': True,  # Include site-packages
+    'bootstrap': 'custom_bootstrap',  # Use custom bootstrap
+    'optimize': 0,  # Don't optimize - helps with debugging
 }
 
 setup(
