59caf3edcc24bb93688e85ed109bbc326620d43d
Atrone <anthony.teixeira55@gmail.com>
2025-04-24 15:17:10 -0400

    asda

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@ from setuptools import setup
 APP = ['spy_ema_chad.py']
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['pandas', 'numpy', 'cmath'],
-    'includes': ['cmath','pandas._libs.tslibs', 'pandas._libs.tslibs.timestamps', 'pandas._libs.testing'],
-    'excludes': ['numpy.__config__', 'numpy.distutils', 'numpy.f2py', 'numpy.testing'],
-    'plist': {
-        'CFBundleName': 'MacApp',
-    },
     'optimize': 1,
-    'resources': [],  # Add any files needed
+    'includes': ['numpy', 'pandas'],
+    'packages': [],
+    'plist': {'CFBundleName': 'MacApp'},
 }
 
 setup(
